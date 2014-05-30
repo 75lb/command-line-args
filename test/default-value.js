5de37e3ea38f0a4de31b82011a9d23f8b387bcb8
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-30 10:59:05 +0100

    default values

---
+++ test/default-value.js
@@ -0,0 +1,21 @@
+var test = require("tap").test;
+var parse = require("../lib/command-line-args");
+
+var optionDefinitions = [
+    { name: "verbose", alias: "v", type: Boolean },
+    { name: "dry", alias: "d", type: Boolean },
+    { name: "colour", alias: "c", value: "orange" },
+    { name: "number", alias: "n", type: Number },
+    { name: "files", defaultOption: true },
+    { name: "colours", type: Array },
+    { name: "tramps", type: Array }
+];
+
+test("default colour", function(t){
+    var argv = [ "--verbose" ];
+    t.deepEqual(parse(optionDefinitions, argv), {
+        verbose: true,
+        colour: "orange"
+    });
+    t.end();
+});
