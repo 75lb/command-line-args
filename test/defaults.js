5266b367fc0e36a3c612deaf4acbddce8444c894
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-07-15 11:13:34 +0100

    added unexpectedType option to parse(argv, options)

---
+++ test/defaults.js
@@ -0,0 +1,9 @@
+var test = require("tap").test;
+var cliArgs = require("../lib/command-line-args");
+
+test("parse works with default argv", function(t){
+    var argv = [ "--verbose", "-d", "--colour", "red", "--number", 3 ];
+    
+    var result = cliArgs([]).parse();
+    t.end();
+});
---
+++ test/unexpectedType.js
@@ -0,0 +1,31 @@
+var test = require("tap").test;
+var cliArgs = require("../lib/command-line-args");
+
+var optionDefinitions = [];
+
+test("unexpected type: throws", function(t){
+    var argv = [ "--clive", "hater" ];
+    t.throws(function(){
+        cliArgs(optionDefinitions).parse(argv);
+    });
+    t.end();
+});
+
+test("unexpected type: string", function(t){
+    var argv = [ "--clive", "hater" ];
+    t.deepEqual(cliArgs(optionDefinitions).parse(argv, { unexpectedType: "string" }), {
+        clive: "hater"
+    });
+    
+    t.end();
+});
+
+test("unexpected type: string, no value", function(t){
+    var argv = [ "--clive", "--nigeria" ];
+    t.deepEqual(cliArgs(optionDefinitions).parse(argv, { unexpectedType: "string" }), {
+        clive: null,
+        nigeria: null
+    });
+    
+    t.end();
+});
