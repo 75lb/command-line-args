bed254538c44135b93793ede80296784993efa32
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-06-14 11:56:29 +0300

    remove old tests

--- test/defaults.js
+++
@@ -1,9 +0,0 @@
-var test = require("tape");
-var parse = require("../");
-
-test("parse works with default argv", function(t){
-    var argv = [ "--verbose", "-d", "--colour", "red", "--number", 3 ];
-    
-    var result = cliArgs([]).parse();
-    t.end();
-});
