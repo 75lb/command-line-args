692873cbdb7df052d566788aebee54fd8eed8ed2
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-06-30 19:58:53 +0100

    grouping test passing

--- test/grouping.js
+++ test/grouping.js
@@ -9,7 +9,7 @@ var optionDefinitions = [
 
 test("groups", function(t){
     var cli = cliArgs(optionDefinitions);
-    t.deepEqual(cli.parse([ "--one", "1", "--two", "2", "--three", "3" ]), { 
+    t.deepEqual(cli.parse([ "--one", "1", "--two", "2", "--three", "3" ]), {
         a: {
             one: "1",
             two: "2"
@@ -18,6 +18,38 @@ test("groups", function(t){
             three: "3"
         },
         _none: {},
+        _all: {
+            one: "1",
+            two: "2",
+            three: "3"
+        }
+    });
+
+    t.end();
+});
+
+test("groups: multiple and _none", function(t){
+    var optionDefinitions = [
+        { name: "one", group: ["a", "f"] },
+        { name: "two", group: ["a", "g"] },
+        { name: "three" },
+    ];
+
+    var cli = cliArgs(optionDefinitions);
+    t.deepEqual(cli.parse([ "--one", "1", "--two", "2", "--three", "3" ]), {
+        a: {
+            one: "1",
+            two: "2"
+        },
+        f: {
+            one: "1",
+        },
+        g: {
+            two: "2"
+        },
+        _none: {
+            three: "3"
+        },
         _all: {
             one: "1",
             two: "2",
