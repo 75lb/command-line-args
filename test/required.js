b69a51c27757196ddfd53839357b3197d651395b
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-07-10 13:13:06 +0100

    required option now requires an array to have length

--- test/required.js
+++ test/required.js
@@ -1,12 +1,11 @@
 var test = require("tape");
 var cliArgs = require("../");
 
-var cliOptions = [
-    { name: "one" },
-    { name: "two", required: true }
-];
-
-test("required: throws", function(t){
+test("required: throws on missing required value", function(t){
+    var cliOptions = [
+        { name: "one" },
+        { name: "two", required: true }
+    ];
     var argv = [ "--one", "what" ];
     t.throws(function(){
         var result = cliArgs(cliOptions).parse(argv);
@@ -14,10 +13,38 @@ test("required: throws", function(t){
     t.end();
 });
 
-test("required: does not throw", function(t){
+test("required: does not throw when required value supplied", function(t){
+    var cliOptions = [
+        { name: "one" },
+        { name: "two", required: true }
+    ];
     var argv = [ "--one", "what", "--two" ];
     t.doesNotThrow(function(){
         var result = cliArgs(cliOptions).parse(argv);
     });
+    t.end();
+});
+
+test("required: does not throw with default value", function(t){
+    var cliOptions = [
+        { name: "one" },
+        { name: "two", required: true, value: 2 }
+    ];
+    var argv = [ "--one", "what" ];
+    t.doesNotThrow(function(){
+        var result = cliArgs(cliOptions).parse(argv);
+    });
+    t.end();
+});
+
+test("required: throws on empty array", function(t){
+    var cliOptions = [
+        { name: "one" },
+        { name: "two", required: true, value: [] }
+    ];
+    var argv = [ "--one", "what" ];
+    t.throws(function(){
+        var result = cliArgs(cliOptions).parse(argv);
+    }, /invalid/i);
     t.end();
 });
