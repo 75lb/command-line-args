d3afb7d919e6b13070195098849d07a285688adc
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-07-25 18:54:18 +0100

    docs

---
+++ example/five.js
@@ -0,0 +1,4 @@
+module.exports = [
+    { name: "files", type: String, multiple: true, value: [ "one.js" ] },
+    { name: "max", type: Number, value: 3 }
+];
---
+++ example/group.js
@@ -0,0 +1,4 @@
+module.exports = [
+    { name: "verbose", alias: "v", type: Boolean, group: "standard" },
+    { name: "help", alias: "h", type: Boolean, group: "standard" }
+];
