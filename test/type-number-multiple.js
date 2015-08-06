bea0868ac48693e65fc3d1112f461e9bf2afd582
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-08-06 19:18:34 +0100

    'multiple' now works with Boolean

--- test/type-number-multiple.js
+++ test/type-number-multiple.js
@@ -5,9 +5,21 @@ var optionDefinitions = [
     { name: "array", type: Number, multiple: true },
 ];
 
-var argv = [ "--array", "1", "2", "3" ];
 
-test("multiple: true", function(t){
+test("number multiple: 1", function(t){
+    var argv = [ "--array", "1", "2", "3" ];
+    var result = cliArgs(optionDefinitions).parse(argv);
+    t.deepEqual(result, {
+        array: [ 1, 2, 3 ]
+    });
+    t.notDeepEqual(result, {
+        array: [ "1", "2", "3" ]
+    });
+    t.end();
+});
+
+test("number multiple: 2", function(t){
+    var argv = [ "--array", "1", "--array", "2", "--array", "3" ];
     var result = cliArgs(optionDefinitions).parse(argv);
     t.deepEqual(result, {
         array: [ 1, 2, 3 ]
