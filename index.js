c35ca35f0c52b81d5a960b1250136801b4a5e7d3
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-10-11 16:27:15 +0100

    fix index

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var detect = require('feature-detect-es6')
 
 module.exports = detect.class() && detect.arrowFunction()
   ? require('./lib/command-line-args')
-  : require('./lib/es5-compiled')
+  : require('./es5/command-line-args')
