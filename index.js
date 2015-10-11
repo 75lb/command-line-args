282c313f39b537690463ef8d4e24ef8b58097ff7
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-10-11 19:28:49 +0100

    upgraded feature-detect-es6

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var detect = require('feature-detect-es6')
 
-module.exports = detect.class() && detect.arrowFunction()
+module.exports = detect.class() && detect.arrowFunction() && detect.newArrayFeatures()
   ? require('./lib/command-line-args')
   : require('./es5/command-line-args')
