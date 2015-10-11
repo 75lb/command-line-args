50d35e4c0b565c2f363a9ac36e8168537c1d7339
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-10-11 18:48:57 +0100

    add babel polyfill to give Array.prototype.find support in ES5

--- es5/command-line-args.js
+++ es5/command-line-args.js
@@ -4,6 +4,7 @@ var _createClass = (function () { function defineProperties(target, props) { for
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
+require("babel/polyfill");
 var a = require('array-tools');
 var o = require('object-tools');
 var Definitions = require('./definitions');
