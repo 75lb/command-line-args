70e1c785e6f68245e0c19e4b93221b9f93ab8c58
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-11-11 21:54:02 +0000

    clone process.argv, leave the original untouched - fixes #14

--- test/detect-process-argv.js
+++ test/detect-process-argv.js
@@ -8,3 +8,12 @@ test('detect process.argv: should automatically remove first two argv items', fu
   })
   t.end()
 })
+
+test('process.argv is left untouched', function (t) {
+  process.argv = [ 'node', 'filename', '--one', 'eins' ]
+  t.deepEqual(cliArgs({ name: 'one' }).parse(), {
+    one: 'eins'
+  })
+  t.deepEqual(process.argv, [ 'node', 'filename', '--one', 'eins' ])
+  t.end()
+})
