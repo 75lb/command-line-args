02ea9a55d61c3232d76e1db958f1d243e75f6c28
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-12-23 10:10:03 +0000

    removed core-js dependency. Fixes #33

--- test.js
+++ test.js
@@ -2,10 +2,6 @@
 var detect = require('feature-detect-es6')
 var TestRunner = require('test-runner')
 
-if (!detect.newArrayFeatures()) {
-  require('core-js/es6/array')
-}
-
 if (detect.all('class', 'arrowFunction', 'let', 'const')) {
   TestRunner.run('src/test/*.js')
 } else {
