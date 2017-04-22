f16b8c8195532ce8f0de840ea3e033554c75d9c5
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-04-22 12:28:50 +0100

    added more defaultValue tests

--- test/default-value.js
+++ test/default-value.js
@@ -89,3 +89,57 @@ runner.test('default value: combined with defaultOption', function () {
     path: './'
   })
 })
+
+runner.test('default value: combined with multiple and defaultOption', function () {
+  const defs = [
+    { name: 'path', multiple: true, defaultOption: true, defaultValue: './' },
+  ]
+
+  let argv = [ '--path', 'test1', 'test2' ]
+  a.deepStrictEqual(commandLineArgs(defs, { argv }), {
+    path: [ 'test1', 'test2' ]
+  })
+  argv = [ '--path', 'test' ]
+  a.deepStrictEqual(commandLineArgs(defs, { argv }), {
+    path: [ 'test' ]
+  })
+  argv = [ 'test1', 'test2' ]
+  a.deepStrictEqual(commandLineArgs(defs, { argv }), {
+    path: [ 'test1', 'test2' ]
+  })
+  argv = [ 'test' ]
+  a.deepStrictEqual(commandLineArgs(defs, { argv }), {
+    path: [ 'test' ]
+  })
+  argv = [ ]
+  a.deepStrictEqual(commandLineArgs(defs, { argv }), {
+    path: [ './' ]
+  })
+})
+
+runner.test('default value: array default combined with multiple and defaultOption', function () {
+  const defs = [
+    { name: 'path', multiple: true, defaultOption: true, defaultValue: [ './' ] },
+  ]
+
+  let argv = [ '--path', 'test1', 'test2' ]
+  a.deepStrictEqual(commandLineArgs(defs, { argv }), {
+    path: [ 'test1', 'test2' ]
+  })
+  argv = [ '--path', 'test' ]
+  a.deepStrictEqual(commandLineArgs(defs, { argv }), {
+    path: [ 'test' ]
+  })
+  argv = [ 'test1', 'test2' ]
+  a.deepStrictEqual(commandLineArgs(defs, { argv }), {
+    path: [ 'test1', 'test2' ]
+  })
+  argv = [ 'test' ]
+  a.deepStrictEqual(commandLineArgs(defs, { argv }), {
+    path: [ 'test' ]
+  })
+  argv = [ ]
+  a.deepStrictEqual(commandLineArgs(defs, { argv }), {
+    path: [ './' ]
+  })
+})
