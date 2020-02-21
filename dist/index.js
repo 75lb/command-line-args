f6abdcc4b270d4274238a618f49288973ac447d0
Lloyd Brookes <lloyd.brookes@gmail.com>
2020-02-21 09:42:25 +0000

    upgrade deps

--- dist/index.js
+++ dist/index.js
@@ -68,95 +68,23 @@
   }
 
   /**
-   * Takes any input and guarantees an array back.
-   *
-   * - converts array-like objects (e.g. `arguments`) to a real array
-   * - converts `undefined` to an empty array
-   * - converts any another other, singular value (including `null`) into an array containing that value
-   * - ignores input which is already an array
-   *
-   * @module array-back
-   * @example
-   * > const arrayify = require('array-back')
-   *
-   * > arrayify(undefined)
-   * []
-   *
-   * > arrayify(null)
-   * [ null ]
-   *
-   * > arrayify(0)
-   * [ 0 ]
-   *
-   * > arrayify([ 1, 2 ])
-   * [ 1, 2 ]
-   *
-   * > function f(){ return arrayify(arguments); }
-   * > f(1,2,3)
-   * [ 1, 2, 3 ]
-   */
-
-  function isObject$1 (input) {
-    return typeof input === 'object' && input !== null
-  }
-
-  function isArrayLike$1 (input) {
-    return isObject$1(input) && typeof input.length === 'number'
-  }
-
-  /**
-   * @param {*} - the input value to convert to an array
-   * @returns {Array}
-   * @alias module:array-back
-   */
-  function arrayify$1 (input) {
-    if (Array.isArray(input)) {
-      return input
-    } else {
-      if (input === undefined) {
-        return []
-      } else if (isArrayLike$1(input)) {
-        return Array.prototype.slice.call(input)
-      } else {
-        return [ input ]
-      }
-    }
-  }
-
-  /**
-   * Find and either replace or remove items in an array.
-   *
    * @module find-replace
-   * @example
-   * > const findReplace = require('find-replace')
-   * > const numbers = [ 1, 2, 3]
-   *
-   * > findReplace(numbers, n => n === 2, 'two')
-   * [ 1, 'two', 3 ]
-   *
-   * > findReplace(numbers, n => n === 2, [ 'two', 'zwei' ])
-   * [ 1, [ 'two', 'zwei' ], 3 ]
-   *
-   * > findReplace(numbers, n => n === 2, 'two', 'zwei')
-   * [ 1, 'two', 'zwei', 3 ]
-   *
-   * > findReplace(numbers, n => n === 2) // no replacement, so remove
-   * [ 1, 3 ]
    */
 
   /**
    * @param {array} - The input array
-   * @param {testFn} - A predicate function which, if returning `true` causes the current item to be operated on.
-   * @param [replaceWith] {...any} - If specified, found values will be replaced with these values, else removed.
+   * @param {function} - A predicate function which, if returns `true` causes the current item to be operated on.
+   * @param [replaceWith] {...any} - If specified, each found value will be replaced with these values, else removed. If a `replaceWith` value is a function, it will be invoked with the found item and its result used as the replace value.
    * @returns {array}
    * @alias module:find-replace
    */
-  function findReplace (array, testFn) {
+  function findReplace (array, findFn, ...replaceWiths) {
     const found = [];
-    const replaceWiths = arrayify$1(arguments);
-    replaceWiths.splice(0, 2);
+    if (!Array.isArray(array)) {
+      throw new Error('Input must be an array')
+    }
 
-    arrayify$1(array).forEach((value, index) => {
+    for (const [index, value] of array.entries()) {
       let expanded = [];
       replaceWiths.forEach(replaceWith => {
         if (typeof replaceWith === 'function') {
@@ -166,18 +94,18 @@
         }
       });
 
-      if (testFn(value)) {
+      if (findFn(value)) {
         found.push({
           index: index,
           replaceWithValue: expanded
         });
       }
-    });
+    }
 
-    found.reverse().forEach(item => {
-      const spliceArgs = [ item.index, 1 ].concat(item.replaceWithValue);
+    for (const item of found.reverse()) {
+      const spliceArgs = [item.index, 1].concat(item.replaceWithValue);
       array.splice.apply(array, spliceArgs);
-    });
+    }
 
     return array
   }
@@ -421,8 +349,8 @@
    *   // prints `true`
    * }
    */
-  function isArrayLike$2 (input) {
-    return isObject$2(input) && typeof input.length === 'number'
+  function isArrayLike$1 (input) {
+    return isObject$1(input) && typeof input.length === 'number'
   }
 
   /**
@@ -431,7 +359,7 @@
    * @returns {boolean}
    * @static
    */
-  function isObject$2 (input) {
+  function isObject$1 (input) {
     return typeof input === 'object' && input !== null
   }
 
@@ -598,8 +526,8 @@
   var t = {
     isNumber,
     isPlainObject,
-    isArrayLike: isArrayLike$2,
-    isObject: isObject$2,
+    isArrayLike: isArrayLike$1,
+    isObject: isObject$1,
     isDefined,
     isUndefined,
     isNull,
